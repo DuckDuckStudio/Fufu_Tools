e485d75401471a3fd605b8513f4d56a7f092fc21
Duck Studio <Yzcbs123@163.com>
2023-07-15 03:56:11 +0800

    解决了Issues #1

--- 强制删除文件夹/强制删除文件夹.bat
+++ 强制删除文件夹/强制删除文件夹.bat
@@ -1,7 +1,15 @@
 @echo off
 set /p folder="请输入要删除的文件夹路径(不要包含引号)："
+:start
 takeown /f "%folder%" /r /d y
 icacls "%folder%" /grant administrators:F /t
 rd /s /q "%folder%"
-echo 删除完成！
+REM 检查文件夹是否任然存在
+if exist "%folder%" (
+    echo "检测到文件夹删除失败，正在重新尝试"
+    goto :start
+    REM 跳转到标签start
+) else (
+    echo "删除完成"
+)
 pause
