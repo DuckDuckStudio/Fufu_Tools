ee0a943885cdfa90e6d1c5722af4e0bbc7c54bd3
Duck Studio <Yzcbs123@163.com>
2023-07-23 16:30:26 +0800

    修复了文本显示错误

--- C盘缓存清理/清C盘缓存.bat
+++ C盘缓存清理/清C盘缓存.bat
@@ -1,6 +1,8 @@
 @echo off
 REM 关闭回显
 REM 删除临时文件夹
+chcp 65001
+REM 使用UTF-8编码
 echo "正在删除缓存文件"
 rd /s /q C:\Users\%USERNAME%\AppData\Local\Temp\*.*
 rd /s /q C:\Documents and Settings\%USERNAME%\Local Settings\temp\*.*
