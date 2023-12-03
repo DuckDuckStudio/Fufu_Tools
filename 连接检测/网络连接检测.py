e362abfd49173679e280c47d26a219ab5a3e8901
Duck Studio <Yzcbs123@163.com>
2023-12-03 23:37:50 +0800

    简化原小工具文件夹名称，并添加国内连接检测

---
+++ 连接检测/网络连接检测.py
@@ -0,0 +1,34 @@
+import requests
+import time
+import os
+
+def check_wlan():
+    headers = {
+        'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.3'
+    }
+    try:
+        response = requests.get("https://www.bilibili.com/", headers=headers)
+        if response.status_code == 200:
+            print("Connection is successful!")
+        else:
+            print("Connection failed with status code:", response.status_code)
+    except Exception as e:
+        print("Connection failed!")
+        print("Error:", e)
+
+#---
+
+while True:
+    print("Attempting to connect...")
+    print("-------------------")
+
+    check_wlan() # checking connection
+
+    # Wait for next check
+    print("-------------------")
+    for i in range(10, 0, -1):
+        print("\rNext check in {}s...".format(i), end="")
+        time.sleep(1)
+
+    # Cross-platform method to clear the console
+    os.system('cls' if os.name == 'nt' else 'clear')
