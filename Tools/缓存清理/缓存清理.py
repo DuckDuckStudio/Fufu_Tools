d1107789a88eb56b30fc6fcb7ef0a70d8109588f
鸭鸭 <yzcbs123@163.com>
2024-03-16 20:32:49 +0800

    Issues #11

--- Tools/缓存清理/缓存清理.py
+++ Tools/缓存清理/缓存清理.py
@@ -1,6 +1,15 @@
 import os
 from tqdm import tqdm
 import subprocess
+import configparser
+
+# READ config.ini file
+script_dir = os.path.dirname(os.path.realpath(__file__))
+config_file_path = os.path.join(script_dir, "config.ini")
+config = configparser.ConfigParser()
+config.read(config_file_path)
+ctr = config.getboolean('settings', 'customer_temp_folder')
+ac_ctr = ctr = config.getboolean('settings', 'always_clear_ctr')
 
 print("正在删除缓存文件...")
 
@@ -11,6 +20,33 @@ tmp_path = os.environ["TMP"]
 
 # 获取所有缓存文件路径
 file_paths = []
+# -- Check --
+if ctr:# 有任意值且不为False则为True
+    print("检测到自定义缓存文件夹，是否同步清理？")
+    while not ac_ctr:
+        print("[Y]是 [N]否 [A]总是")
+        temp = input("你的回答是：")
+        if temp.lower() in ["y", "yes", "是"]:
+            for root, dirs, files in os.walk(ctr):
+                for file in files:
+                    file_paths.append(os.path.join(root, file))
+            break
+        elif temp.lower() in ["n", "no", "否", "不"]:
+            break
+        elif temp.lower() in ["a", "always", "总是"]:
+            print("设置中...")
+            # 修改ini文件配置
+            config["settings"]["always_clear_ctr"] = "True"
+            with open(config_file_path, 'w') as configfile:
+                config.write(configfile)
+            print("设置完成！")
+            for root, dirs, files in os.walk(ctr):
+                for file in files:
+                    file_paths.append(os.path.join(root, file))
+            break
+        else:
+            print("请按说明回答！")
+
 for root, dirs, files in os.walk(temp_path):
     for file in files:
         file_paths.append(os.path.join(root, file))
