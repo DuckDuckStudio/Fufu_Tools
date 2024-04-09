b4e58f2c5ff59cb3a81b5ea5fec00a9df90003b6
鸭鸭「カモ」 <Yzcbs123@163.com>
2024-04-09 11:57:07 +0800

    Fix Issues #22

--- Tools/【实验性工具】/自动化/自动按键连点.py
+++ Tools/【实验性工具】/自动化/自动按键连点.py
@@ -2,12 +2,49 @@ import pyautogui
 import keyboard
 import time
 
+# 有效的按键
+# 依据文档：https://pyautogui.readthedocs.io/en/latest/keyboard.html#keyboard-keys
+valid_keys = [
+    '\t', '\n', '\r', ' ', '!', '"', '#', '$', '%', '&', "'", '(',
+    ')', '*', '+', ',', '-', '.', '/', '0', '1', '2', '3', '4', '5', '6', '7',
+    '8', '9', ':', ';', '<', '=', '>', '?', '@', '[', '\\', ']', '^', '_', '`',
+    'a', 'b', 'c', 'd', 'e','f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o',
+    'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', '{', '|', '}', '~',
+    'accept', 'add', 'alt', 'altleft', 'altright', 'apps', 'backspace',
+    'browserback', 'browserfavorites', 'browserforward', 'browserhome',
+    'browserrefresh', 'browsersearch', 'browserstop', 'capslock', 'clear',
+    'convert', 'ctrl', 'ctrlleft', 'ctrlright', 'decimal', 'del', 'delete',
+    'divide', 'down', 'end', 'enter', 'esc', 'escape', 'execute', 'f1', 'f10',
+    'f11', 'f12', 'f13', 'f14', 'f15', 'f16', 'f17', 'f18', 'f19', 'f2', 'f20',
+    'f21', 'f22', 'f23', 'f24', 'f3', 'f4', 'f5', 'f6', 'f7', 'f9',# 手动去除 f8 键
+    'final', 'fn', 'hanguel', 'hangul', 'hanja', 'help', 'home', 'insert', 'junja',
+    'kana', 'kanji', 'launchapp1', 'launchapp2', 'launchmail',
+    'launchmediaselect', 'left', 'modechange', 'multiply', 'nexttrack',
+    'nonconvert', 'num0', 'num1', 'num2', 'num3', 'num4', 'num5', 'num6',
+    'num7', 'num8', 'num9', 'numlock', 'pagedown', 'pageup', 'pause', 'pgdn',
+    'pgup', 'playpause', 'prevtrack', 'print', 'printscreen', 'prntscrn',
+    'prtsc', 'prtscr', 'return', 'right', 'scrolllock', 'select', 'separator',
+    'shift', 'shiftleft', 'shiftright', 'sleep', 'space', 'stop', 'subtract', 'tab',
+    'up', 'volumedown', 'volumemute', 'volumeup', 'win', 'winleft', 'winright', 'yen',
+    'command', 'option', 'optionleft', 'optionright'
+]
+
 print("本程序用于自动连按键盘上的按键，左键连点出门左转。")
 
 # 从用户那里获取连续按键间隔时间，并转换为浮点数
 click_interval = float(input("请输入每次按键的间隔时间(秒)："))  # 转换为浮点数
-key = input("请输入需要连按的按键：")
-key = key.lower()
+
+# 获取需要连按的按键
+while True:
+    key = input("请输入需要连按的按键：")
+    key = key.lower()
+    if key in valid_keys:
+        break
+    elif key == "f8":
+        print("[WARN]需要自动连按的按键与开启自动连按的按键相同，无法使用！")
+        print("请重新输入一个其他按键！")
+    else:
+        print("输入的按键无效！请重新输入！")
 
 # 控制自动按键的开关
 auto_pressing = False
