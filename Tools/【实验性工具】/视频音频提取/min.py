c9bd4cef0f4b95820b679b61b708b09f63aa90e1
鸭鸭「カモ」 <Yzcbs123@163.com>
2024-05-04 03:49:09 +0800

    更新输入处理

--- Tools/【实验性工具】/视频音频提取/min.py
+++ Tools/【实验性工具】/视频音频提取/min.py
@@ -29,21 +29,25 @@ def extract_audio(video_path, audio_path):
 
 # 视频文件路径
 video_path_input = input("视频文件的完整路径是：")
-video_path = video_path_input.strip('\"')
+if video_path_input.startswith(("'", '"')) and video_path_input.endswith(("'", '"')):
+    video_path_input = video_path_input[1:-1]# 移除引号
 
 # 获取视频文件名（不带扩展名）
-video_name = os.path.splitext(os.path.basename(video_path))[0]
+video_name = os.path.splitext(os.path.basename(video_path_input))[0]
 
 # 音频文件路径
 audio_path_input = input("需要将提取后的音频文件输出到哪里：")
-audio_path = audio_path_input.strip('\"')
+if audio_path_input.startswith(("'", '"')) and audio_path_input.endswith(("'", '"')):
+    audio_path_input = audio_path_input[1:-1]# 移除引号
+if not audio_path_input.endswith('\\'):
+    audio_path_input += '\\'# 添加结尾的斜杠
 
 # 如果输出路径是文件夹，则使用视频文件名作为音频文件名
-if os.path.isdir(audio_path):
-    audio_path = os.path.join(audio_path, f"{video_name}.mp3")
+if os.path.isdir(audio_path_input):
+    audio_path = os.path.join(audio_path_input, f"{video_name}.mp3")
 
 print("[INFO]目前配置信息：\n视频文件路径：", video_path_input, "\n输出音频文件的路径：", audio_path)
 
 # 调用函数提取音频
-extract_audio(video_path, audio_path)
+extract_audio(video_path_input, audio_path)
 
