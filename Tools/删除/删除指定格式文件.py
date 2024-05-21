cd217f5dbaddb3b8aea4aebc63c418cbff4e2c60
鸭鸭「カモ」 <Yzcbs123@163.com>
2024-05-22 01:46:44 +0800

    perf:优化输出

--- Tools/删除/删除指定格式文件.py
+++ Tools/删除/删除指定格式文件.py
@@ -5,7 +5,7 @@ init(autoreset=True)  # 初始化 Colorama，使颜色输出生效
 acount = 0
 countd = 0
 
-folder_path = input(f"请输入文件夹路径:")
+folder_path = input("请输入文件夹路径:")
 
 if folder_path.startswith(("'", '"')) and folder_path.endswith(("'", '"')):
     folder_path = folder_path[1:-1]
@@ -14,11 +14,11 @@ if not folder_path.endswith('\\'):
     folder_path += '\\'
 
 if not os.path.exists(folder_path):
-    print(f"\r{Fore.RED}✕{Fore.RESET} 指定的目录路径不存在，请重新运行程序并输入有效的目录路径。")
+    print(f"{Fore.RED}✕{Fore.RESET} 指定的目录路径不存在，请重新运行程序并输入有效的目录路径。")
     input("按 ENTER 键继续...")
     exit()
 
-print(f"\r{Fore.GREEN}✓{Fore.RESET} 选取的文件夹路径: {folder_path}")
+print(f"{Fore.GREEN}✓{Fore.RESET} 选取的文件夹路径: {folder_path}")
 
 # --------
 
@@ -27,7 +27,7 @@ file_extension = input("请输入你要查找文件的格式：")
 if not file_extension.startswith('.'):
     file_extension = '.' + file_extension
 
-print(f"\r{Fore.GREEN}✓{Fore.RESET} 要删除的文件格式: {file_extension}")
+print(f"{Fore.GREEN}✓{Fore.RESET} 要删除的文件格式: {file_extension}")
 
 # -------- 获取参数 ↑ | 操作 ↓ ---------
 
@@ -46,8 +46,8 @@ for root, dirs, files in os.walk(folder_path):
             file_path = os.path.join(root, file)
             countd = countd + 1
             os.remove(file_path)
-            print(f'已删除文件: {file_path} (还剩{acount-countd}个文件)')
+            print(f'已删除文件: {file_path} (还剩 {Fore.BLUE} {acount-countd} {Fore.RESET} 个文件)')
 
-print(f"文件删除完成！总共删除了{countd}个原文件")
+print(f"{Fore.GREEN}✓ 文件删除完成！{Fore.RESET}总共删除了 {Fore.BLUE} {countd} {Fore.RESET} 个原文件")
 
 input ("按 Enter 键退出...")
