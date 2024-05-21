570ddb229112608531d83610afc9cd70a0a55fe5
鸭鸭「カモ」 <Yzcbs123@163.com>
2024-05-22 01:29:00 +0800

    feat:新增 删除指定格式文件 工具

---
+++ Tools/删除/删除指定格式文件.py
@@ -0,0 +1,53 @@
+import os
+from colorama import init, Fore
+
+init(autoreset=True)  # 初始化 Colorama，使颜色输出生效
+acount = 0
+countd = 0
+
+folder_path = input(f"请输入文件夹路径:")
+
+if folder_path.startswith(("'", '"')) and folder_path.endswith(("'", '"')):
+    folder_path = folder_path[1:-1]
+
+if not folder_path.endswith('\\'):
+    folder_path += '\\'
+
+if not os.path.exists(folder_path):
+    print(f"\r{Fore.RED}✕{Fore.RESET} 指定的目录路径不存在，请重新运行程序并输入有效的目录路径。")
+    input("按 ENTER 键继续...")
+    exit()
+
+print(f"\r{Fore.GREEN}✓{Fore.RESET} 选取的文件夹路径: {folder_path}")
+
+# --------
+
+file_extension = input("请输入你要查找文件的格式：")
+
+if not file_extension.startswith('.'):
+    file_extension = '.' + file_extension
+
+print(f"\r{Fore.GREEN}✓{Fore.RESET} 要删除的文件格式: {file_extension}")
+
+# -------- 获取参数 ↑ | 操作 ↓ ---------
+
+for root, dirs, files in os.walk(folder_path):
+    for file in files:
+        if file.endswith(file_extension):
+            file_path = os.path.join(root, file)
+            acount = acount + 1
+
+print(f"一共找到 {Fore.BLUE} {acount} {Fore.RESET} 个符合条件的文件。")
+
+# 删除指定格式的文件
+for root, dirs, files in os.walk(folder_path):
+    for file in files:
+        if file.endswith(file_extension):
+            file_path = os.path.join(root, file)
+            countd = countd + 1
+            os.remove(file_path)
+            print(f'已删除文件: {file_path} (还剩{acount-countd}个文件)')
+
+print(f"文件删除完成！总共删除了{countd}个原文件")
+
+input ("按 Enter 键退出...")
