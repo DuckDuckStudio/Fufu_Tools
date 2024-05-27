529f5b0907f03ffa9b7679bb541eb2682dfb0870
鸭鸭「カモ」 <Yzcbs123@163.com>
2024-05-27 11:15:48 +0800

    perf:使其可以自定义 --code-file-formats 参数的内容（在config.ini中）

--- Tools/开发工具/统计/代码总行数.py
+++ Tools/开发工具/统计/代码总行数.py
@@ -1,5 +1,6 @@
 import os
 import argparse
+import configparser
 from colorama import init, Fore
 
 init(autoreset=True)
@@ -33,7 +34,7 @@ def count_lines_in_file(file_path, ignore_empty_lines=False, ignore_comments=Fal
         print(f"{Fore.RED}✕{Fore.RESET} 没有权限访问文件: {Fore.BLUE}{file_path}{Fore.RESET}")
         return 0, 0, 0
     except Exception as e:
-        print(f"{Fore.RED}✕{Fore.RESET} 出现未知错误: {Fore.RED}{e}{Fore.RESET}")
+        print(f"{Fore.RED}✕{Fore.RESET} 文件 {Fore.BLUE}{file_path}{Fore.RESET} 出现未知错误: {Fore.RED}{e}{Fore.RESET}")
         return 0, 0, 0
 
 if __name__ == '__main__':
@@ -46,14 +47,27 @@ if __name__ == '__main__':
     parser.add_argument('--ignore-comments', action='store_true', help='在统计时忽略注释行', required=False, dest="ignore_comments")
     parser.add_argument('--show-ignored-lines', action='store_true', help='显示总共忽略的空行和注释行数', required=False, dest="show_ignored_lines")
     parser.add_argument('--show-test-info', action='store_true', help='显示测试信息（非必要不要使用）', required=False, dest="show_test_info")
+    parser.add_argument('--code-file-formats', action='store_true', help='仅统计代码文件', required=False, dest="code_file_formats")
 
     args = parser.parse_args()
 
+    os.chdir(os.path.dirname(os.path.abspath(__file__)))
+    config = configparser.ConfigParser()
+    config.read('..\config.ini')
+    code_file_extensions = config.get('Statistics', 'CodeFileExtensions')
+
     ignore_folders = args.ignore_folders.split(',') if args.ignore_folders else []
     ignore_files = args.ignore_files.split(',') if args.ignore_files else []
-    file_formats = args.file_formats.split(',') if args.file_formats else []
 
-
+    if args.file_formats and args.code_file_formats:
+        print(f"{Fore.RED}✕{Fore.RESET} --formats 不得与 --code-file-formats 同时使用")
+        exit()
+    elif args.file_formats:
+        file_formats = args.file_formats.split(',')
+    elif args.code_file_formats:
+        file_formats = code_file_extensions.split(',')
+    else:
+        file_formats = []
 
     total_lines = 0
     ignored_empty_lines = 0
@@ -67,7 +81,11 @@ if __name__ == '__main__':
         dirs[:] = [d for d in dirs if d not in ignore_folders]
         for file in files:
             if file not in ignore_files:
+                if args.show_test_info:
+                    print(f"{Fore.YELLOW}[TEST]{Fore.RESET} 文件 {Fore.BLUE}{file}{Fore.RESET} {Fore.GREEN}未被{Fore.YELLOW}忽略文件{Fore.RESET}参数忽略")
                 if not file_formats or file.endswith(tuple(file_formats)):
+                    if args.show_test_info:
+                        print(f"{Fore.YELLOW}[TEST]{Fore.RESET} 文件 {Fore.BLUE}{file}{Fore.RESET} {Fore.GREEN}未被{Fore.YELLOW}指定格式{Fore.RESET}参数忽略 ({not args.file_formats} | {file.endswith(tuple(file_formats))})")
                     file_path = os.path.join(root, file)
                     lines = count_lines_in_file(file_path, args.ignore_empty_lines, args.ignore_comments, args.show_ignored_lines, args.show_test_info)
                     if args.show_ignored_lines:
