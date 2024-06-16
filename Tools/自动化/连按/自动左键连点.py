d391ae107db3fe0bdfc42c542f9081c83dc11cef
鸭鸭「カモ」 <Yzcbs123@163.com>
2024-06-16 12:07:16 +0800

    [style] 统一输出风格 (#64)
    * style: 统一输出风格
    
    ASK:
    应该要把旧的输出统一为新风格吗？我担心以后可能会有其他风格，再进行统一会比较麻烦。
    @Luna-Grace
    
    * style:修改输出样式
    
    * Revert "style:修改输出样式"
    
    This reverts commit d29fe52cb41bdb422f46beeb1e8c63b00d365be6.

--- Tools/自动化/连按/自动左键连点.py
+++ Tools/自动化/连按/自动左键连点.py
@@ -1,13 +1,16 @@
 import pyautogui
 import keyboard
 import time
+from colorama import init, Fore
+
+init(autoreset=True)
 
 while True:
     try:
         click_interval = float(input("请输入每次点击的间隔时间(秒)："))
         break
     except ValueError:
-        print("输入的数据不合法，请重新输入。")
+        print(f"{Fore.RED}✕{Fore.RESET} 输入的数据不合法，请重新输入。")
 
 # 控制自动点击的开关
 auto_clicking = False
@@ -21,13 +24,13 @@ def toggle_auto_click():
     # 切换自动点击状态
     auto_clicking = not auto_clicking
     if first_time:
-        print("自动点击 已开始。")# 首次按下F8必定是开始自动点击
+        print("自动点击 已开始", end="")# 首次按下F8必定是开始自动点击
         first_time = False # 标记为非首次
     else:
         if auto_clicking:
-            print('\033[1A\033[K' + "自动点击 已开始。")
+            print("\r自动点击 已开始", end="")
         else:
-            print('\033[1A\033[K' + "自动点击 已停止。")
+            print("\r自动点击 已停止", end="")
 
 def auto_click():
     while True:
@@ -40,6 +43,6 @@ def auto_click():
 # 绑定F8键，用于切换自动点击状态
 keyboard.add_hotkey('F8', toggle_auto_click)
 
-print("程序已启动，按下F8键开始/停止自动左键连续点击。")
-print("直接关闭窗口可退出本程序。")
+print(f"{Fore.GREEN}✓{Fore.RESET} 程序已启动，按下F8键开始/停止自动左键连续点击。")
+print(f"{Fore.BLUE}[!]{Fore.RESET} 按Ctrl+C可结束程序...")
 auto_click()
