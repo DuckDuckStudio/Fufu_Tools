b016dd3187e1bf32943c633486f8a338b5a5efcf
鸭鸭「カモ」 <Yzcbs123@163.com>
2024-07-25 14:22:26 +0800

    pref: 在某个仓库中执行git相关自动化时可以省去选择仓库目录的步骤 ( DuckDuckStudio/Fufu_Dev_Tools#6 )
    Sync commit:
    * pref: 在某个仓库中执行git相关自动化时可以省去选择仓库目录的步骤
    
    * pref: 避免不必要的进程
    
    * fix: 修复路径不是执行处的问题

--- Tools/开发工具/git/git连续尝试.py
+++ Tools/开发工具/git/git连续尝试.py
@@ -1,3 +1,5 @@
+import os
+import sys
 import time
 import subprocess
 import tkinter as tk
@@ -7,8 +9,6 @@ from colorama import init, Fore
 
 # --- init ---
 init(autoreset=True)
-root = tk.Tk()
-root.withdraw()
 # ------------
 
 def run_commits(working_dir, command):
@@ -33,9 +33,17 @@ def is_network_error(stderr): # 判断错误类型
     return False
 
 def main():
-    print(f"{Fore.BLUE}?{Fore.RESET} 请选择仓库目录", end="")
-    working_dir = filedialog.askdirectory()
-    print(f"\r{Fore.GREEN}✓{Fore.RESET} 选择的仓库目录: {working_dir}")
+    result = subprocess.run(['git', 'rev-parse', '--is-inside-work-tree'], stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+
+    if result.returncode == 0:
+        working_dir = os.getcwd()
+        print(f"\r{Fore.GREEN}✓{Fore.RESET} 已自动选择仓库目录: {working_dir}")
+    else:
+        root = tk.Tk()
+        root.withdraw()
+        print(f"{Fore.BLUE}?{Fore.RESET} 请选择仓库目录", end="")
+        working_dir = filedialog.askdirectory()
+        print(f"\r{Fore.GREEN}✓{Fore.RESET} 选择的仓库目录: {working_dir}")
     
     command = input("请输入需要执行的命令: ")
 
