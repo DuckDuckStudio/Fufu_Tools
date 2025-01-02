18c0789330753759be311aa6639a6f26aba6c588
鸭鸭「カモ」 <Yzcbs123@163.com>
2025-01-02 23:20:21 +0800

    Sync: 同步开发工具仓库的修改 (#156)
    fix #97

--- Tools/开发工具/Script/ffdev.ps1
+++ Tools/开发工具/Script/ffdev.ps1
@@ -1,36 +1,38 @@
 param(
-    [string]$program,
-    [string]$command,
-    [string[]]$inputArgs
+    [string]$tool,
+    [Parameter(ValueFromRemainingArguments=$true)]
+    [string[]]$Args
 )
 
 $parentDir = Split-Path -Path $PSScriptRoot -Parent
-$version = "2024.11.27.2000"
+$version = "2025.01.02.2300"
 $flag = 0
 
-if ($program -eq "总调用") {
+if ($tool -eq "总调用") {
     $pythonScript = Join-Path $parentDir "总调用.py"
-} elseif ($program -eq "目录复制") {
-    $pythonScript = Join-Path $parentDir "目录复制.py"
-} elseif ($program -eq "参数查重") {
+} elseif ($tool -eq "目录复制") {
+    $pythonScript = Join-Path $parentDir "目录复制.pyw"
+} elseif ($tool -eq "参数查重") {
     $pythonScript = Join-Path $parentDir "代码校对\参数查重.py"
-} elseif ($program -eq "非UTF-8编码") {
+} elseif ($tool -eq "非UTF-8编码") {
     $pythonScript = Join-Path $parentDir "代码校对\非UTF-8编码.py"
-} elseif ($program -eq "尾随空格") {
+} elseif ($tool -eq "尾随空格") {
     $pythonScript = Join-Path $parentDir "代码校对\尾随空格.py"
-} elseif ($program -eq "需求生成") {
+} elseif ($tool -eq "末尾空行") {
+    $pythonScript = Join-Path $parentDir "代码校对\末尾空行.py"
+} elseif ($tool -eq "需求生成") {
     $pythonScript = Join-Path $parentDir "生成工具\需求生成.py"
-} elseif ($program -eq "代码行数") {
+} elseif ($tool -eq "代码行数") {
     $pythonScript = Join-Path $parentDir "统计\代码总行数.py"
-} elseif ($program -eq "账号切换") {
+} elseif ($tool -eq "账号切换") {
     $pythonScript = Join-Path $parentDir "git\账号切换.py"
-} elseif ($program -eq "连续push") {
+} elseif ($tool -eq "连续push") {
     $pythonScript = Join-Path $parentDir "git\连续push尝试.py"
-} elseif ($program -eq "连续pull") {
+} elseif ($tool -eq "连续pull") {
     $pythonScript = Join-Path $parentDir "git\连续pull尝试.py"
-} elseif ($program -eq "git连续尝试") {
+} elseif ($tool -eq "git连续尝试") {
     $pythonScript = Join-Path $parentDir "git\git连续尝试.py"
-} elseif ($program -in "ver", "版本", "version", "Version", "--version", "--ver", "-v") {
+} elseif ($tool -in "ver", "版本", "version", "Version", "--version", "--ver", "-v") {
     Write-Output "版本: $version"
     Write-Output "安装在: $parentDir"
     $flag = 1
@@ -41,5 +43,5 @@ if ($program -eq "总调用") {
 }
 
 if ($flag -ne 1) {
-    python $pythonScript $command $inputArgs
+    python $pythonScript $Args
 }
