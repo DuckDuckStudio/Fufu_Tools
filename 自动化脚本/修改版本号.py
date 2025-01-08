a7d6c34043a1ac459cb3f3adc5726e490c579839
Luna Grace <106975825+Luna-Grace@users.noreply.github.com>
2025-01-08 13:15:58 +0800

    chore(dir): 整理目录

--- 自动化脚本/修改版本号.py
+++ 自动化脚本/修改版本号.py
@@ -4,13 +4,14 @@ import sys
 # GitHub Action - Windows 竟然只能输出英文 :(
 
 if len(sys.argv) != 3:
-    print("[ERROR] Usage: python xxx.py <New-version> <Type>")
-    # 例如 python xxx.py v1.0.0 lite
+    print("[ERROR] Usage: python xxx.py <New-version> <Type> <Directory>")
+    # 例如 python xxx.py v1.0.0 lite 打包时的目录
     sys.exit(1)
 
 # 获取参数
 新版本号 = sys.argv[1]
 类型 = sys.argv[2]
+目录 = sys.argv[3]
 
 if not 新版本号:
     print("[ERROR] Version cannot be empty.")
@@ -21,6 +22,10 @@ if 类型 not in ['lite', 'code', 'exe', 'pack']:
     print("[TIP] See https://duckduckstudio.github.io/yazicbs.github.io/Tools/Fufu_Tools/versions/ for more information.")
     sys.exit(1)
 
+if not 目录:
+    print("[ERROR] Directory cannot be empty.")
+    sys.exit(1)
+
 # 如果新版本号以 v 开头，去掉 v
 if 新版本号.startswith('v'):
     新版本号 = 新版本号[1:] # 切片
@@ -28,7 +33,7 @@ if 新版本号.startswith('v'):
 
 print(f"[INFO] New version: {新版本号}")
 
-文件 = os.path.join(os.path.dirname(os.path.dirname(os.path.abspath(sys.argv[0]))), "src", "config.ini")
+文件 = os.path.join(目录, "config.ini")
 try:
     # 读取文件内容
     with open(文件, 'r', encoding='utf-8') as f:
@@ -44,7 +49,7 @@ except Exception as e:
     print(f"[ERROR] A error occurred when processing {文件}: {e}")
     sys.exit(1)
 
-文件 = os.path.join(os.path.dirname(os.path.dirname(os.path.abspath(sys.argv[0]))), "src", "Version")
+文件 = os.path.join(目录, "Version")
 try:
     # 读取文件内容
     with open(文件, 'r', encoding='utf-8') as f:
